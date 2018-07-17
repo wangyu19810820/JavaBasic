e65d26e608f84282a1d3f9a84a9be0d1485b5dcb
wangyu19810820 <gadfly0036_cn@sina.com>
2018-07-17 17:08:02 +0800

    spring集成rmi

--- src/main/java/com/rpc/spring/rmi/service/SpringRmiDemoService.java
+++ src/main/java/com/rpc/spring/rmi/service/SpringRmiDemoService.java
@@ -1,5 +1,9 @@
 package com.rpc.spring.rmi.service;
 
+/**
+ * 当Spring封装RMI后，其Service无需继承Remote接口
+ * 只需用普通的Service就可以
+ */
 public interface SpringRmiDemoService {
 
     String sayHello();
--- src/main/java/com/rpc/spring/rmi/service/SpringRmiDemoServiceImpl.java
+++ src/main/java/com/rpc/spring/rmi/service/SpringRmiDemoServiceImpl.java
@@ -2,6 +2,10 @@ package com.rpc.spring.rmi.service;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * 当Spring封装RMI后，其Service无需继承Remote接口
+ * 只需用普通的Service就可以
+ */
 @Component
 public class SpringRmiDemoServiceImpl implements SpringRmiDemoService {
 
